050cb6bf4b3290a16ba0a33a7b436e7a5a0e91c3
Carsten Hammer <carsten.hammer@t-online.de>
2024-10-18 20:18:30 +0200

    fix bug in import that causes static and normal import to be added

--- sandbox_junit_cleanup_test/src/org/sandbox/jdt/ui/tests/quickfix/Java8CleanUpTest.java
+++ sandbox_junit_cleanup_test/src/org/sandbox/jdt/ui/tests/quickfix/Java8CleanUpTest.java
@@ -297,7 +297,6 @@ public class MyTest {
 				
 				import org.junit.jupiter.api.AfterAll;
 				import org.junit.jupiter.api.AfterEach;
-				import org.junit.jupiter.api.Assertions;
 				import org.junit.jupiter.api.BeforeAll;
 				import org.junit.jupiter.api.BeforeEach;
 				import org.junit.jupiter.api.Disabled;
