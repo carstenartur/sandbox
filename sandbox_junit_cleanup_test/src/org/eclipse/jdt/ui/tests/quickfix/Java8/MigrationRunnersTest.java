37221d4daa5572752c8fe4b6a20f5c7c19d01b35
Carsten Hammer <carsten.hammer@t-online.de>
2026-02-24 22:40:03 +0100

    fix test

--- sandbox_junit_cleanup_test/src/org/eclipse/jdt/ui/tests/quickfix/Java8/MigrationRunnersTest.java
+++ sandbox_junit_cleanup_test/src/org/eclipse/jdt/ui/tests/quickfix/Java8/MigrationRunnersTest.java
@@ -227,8 +227,8 @@ public class MyParameterizedTest {
 				package test;
 				import org.junit.jupiter.api.Test;
 				import org.junit.jupiter.api.extension.ExtendWith;
-				import org.springframework.test.context.junit.jupiter.SpringExtension;
 				import org.springframework.beans.factory.annotation.Autowired;
+				import org.springframework.test.context.junit.jupiter.SpringExtension;
 				
 				@ExtendWith(SpringExtension.class)
 				public class MySpringTest {
