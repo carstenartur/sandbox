6bedb1a32f775336f539b5de699f0bbbd11735e8
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-22 07:02:19 +0200

    refactor: use shared enhanced-for capture validator

--- sandbox_functional_converter/src/org/sandbox/jdt/internal/corext/fix/helper/SafeEnhancedForHandler.java
+++ sandbox_functional_converter/src/org/sandbox/jdt/internal/corext/fix/helper/SafeEnhancedForHandler.java
@@ -10,22 +10,15 @@
  *******************************************************************************/
 package org.sandbox.jdt.internal.corext.fix.helper;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.EnhancedForStatement;
-import org.eclipse.jdt.core.dom.IBinding;
-import org.eclipse.jdt.core.dom.IVariableBinding;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.internal.corext.refactoring.structure.CompilationUnitRewrite;
 
@@ -56,7 +49,9 @@ public class SafeEnhancedForHandler extends EnhancedForHandler {
 		Object stored= data.get(visited);
 		if (stored instanceof JdtLoopExtractor.ExtractedLoop extracted && extracted.model != null) {
 			if (isUnsupportedArrayExistingTarget(visited, extracted.model)
-					|| capturesNonEffectivelyFinalLocal(visited, extracted.model)) {
+					|| LambdaCaptureSafety.hasUnsafeCapture(visited.getBody(),
+							liftedAccumulatorNames(visited, extracted.model),
+							visited.getParameter().resolveBinding())) {
 				return;
 			}
 		}
@@ -70,53 +65,7 @@ public class SafeEnhancedForHandler extends EnhancedForHandler {
 				&& !hasFreshAccumulator(loop, collectTerminal.targetVariable());
 	}
 
-	private boolean capturesNonEffectivelyFinalLocal(EnhancedForStatement loop, LoopModel model) {
-		Set<String> localBindingKeys= new HashSet<>();
-		boolean[] incompleteBindings= { !addBindingKey(localBindingKeys, loop.getParameter().resolveBinding()) };
-		loop.getBody().accept(new ASTVisitor() {
-			@Override
-			public boolean visit(VariableDeclarationFragment node) {
-				incompleteBindings[0]|= !addBindingKey(localBindingKeys, node.resolveBinding());
-				return true;
-			}
-
-			@Override
-			public boolean visit(SingleVariableDeclaration node) {
-				incompleteBindings[0]|= !addBindingKey(localBindingKeys, node.resolveBinding());
-				return true;
-			}
-		});
-		if (incompleteBindings[0]) {
-			return true;
-		}
-
-		Set<String> liftedAccumulatorKeys= liftedAccumulatorKeys(loop, model);
-		boolean[] unsafe= { false };
-		loop.getBody().accept(new ASTVisitor() {
-			@Override
-			public boolean visit(SimpleName node) {
-				IBinding binding= node.resolveBinding();
-				if (!(binding instanceof IVariableBinding variableBinding)) {
-					return true;
-				}
-				IVariableBinding declaration= variableBinding.getVariableDeclaration();
-				String key= declaration.getKey();
-				if (key == null) {
-					unsafe[0]= true;
-					return false;
-				}
-				if (!declaration.isField() && !localBindingKeys.contains(key)
-						&& !liftedAccumulatorKeys.contains(key) && !declaration.isEffectivelyFinal()) {
-					unsafe[0]= true;
-					return false;
-				}
-				return true;
-			}
-		});
-		return unsafe[0];
-	}
-
-	private Set<String> liftedAccumulatorKeys(EnhancedForStatement loop, LoopModel model) {
+	private Set<String> liftedAccumulatorNames(EnhancedForStatement loop, LoopModel model) {
 		String targetVariable= null;
 		if (model.getTerminal() instanceof ReduceTerminal reduceTerminal) {
 			targetVariable= reduceTerminal.targetVariable();
@@ -127,23 +76,7 @@ public class SafeEnhancedForHandler extends EnhancedForHandler {
 		if (targetVariable == null || targetVariable.indexOf('.') >= 0) {
 			return Set.of();
 		}
-
-		Set<String> result= new HashSet<>();
-		String expectedName= targetVariable;
-		loop.getBody().accept(new ASTVisitor() {
-			@Override
-			public boolean visit(SimpleName node) {
-				if (expectedName.equals(node.getIdentifier())
-						&& node.resolveBinding() instanceof IVariableBinding variableBinding) {
-					String key= variableBinding.getVariableDeclaration().getKey();
-					if (key != null) {
-						result.add(key);
-					}
-				}
-				return true;
-			}
-		});
-		return result;
+		return Set.of(targetVariable);
 	}
 
 	private boolean hasFreshAccumulator(EnhancedForStatement loop, String targetVariable) {
@@ -161,17 +94,5 @@ public class SafeEnhancedForHandler extends EnhancedForHandler {
 			return false;
 		}
 		return targetVariable.equals(CollectPatternDetector.isEmptyCollectionDeclaration(declaration));
-	}
-
-	private static boolean addBindingKey(Set<String> keys, IVariableBinding binding) {
-		if (binding == null) {
-			return false;
-		}
-		String key= binding.getVariableDeclaration().getKey();
-		if (key == null) {
-			return false;
-		}
-		keys.add(key);
-		return true;
 	}
 }
