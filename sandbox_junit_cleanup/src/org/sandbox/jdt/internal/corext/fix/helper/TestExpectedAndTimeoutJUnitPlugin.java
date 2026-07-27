6c1d2832fc630bc380f6040b9cf4db6ea2bd88ba
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-27 07:16:19 +0200

    Remove unused combined migration import

--- sandbox_junit_cleanup/src/org/sandbox/jdt/internal/corext/fix/helper/TestExpectedAndTimeoutJUnitPlugin.java
+++ sandbox_junit_cleanup/src/org/sandbox/jdt/internal/corext/fix/helper/TestExpectedAndTimeoutJUnitPlugin.java
@@ -23,7 +23,6 @@ import java.util.List;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
-import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.LambdaExpression;
 import org.eclipse.jdt.core.dom.MarkerAnnotation;
