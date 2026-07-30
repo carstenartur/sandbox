56d769fea66a441e96ad6ca54551a6fc4a2af50f
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-30 21:56:22 +0200

    Prove JDT Core filters fail closed

---
+++ sandbox_junit_cleanup_test/src/org/sandbox/jdt/internal/corext/fix/multifile/JdtCoreHarnessFilterRejectionTest.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Carsten Hammer and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.sandbox.jdt.internal.corext.fix.multifile;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import org.eclipse.core.runtime.CoreException;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTRequestor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.internal.corext.dom.IASTSharedValues;
+import org.eclipse.jdt.internal.corext.refactoring.util.RefactoringASTParser;
+import org.eclipse.jdt.junit.JUnitCore;
+
+import org.sandbox.jdt.internal.corext.fix.multifile.JdtCoreHarnessInventory.Family;
+import org.sandbox.jdt.ui.tests.quickfix.rules.AbstractEclipseJava;
+import org.sandbox.jdt.ui.tests.quickfix.rules.EclipseJava17;
+
+/** Proves that filters are not approximated through skipped Jupiter nodes. */
+public class JdtCoreHarnessFilterRejectionTest {
+
+	@RegisterExtension
+	AbstractEclipseJava context= new EclipseJava17();
+
+	private IPackageFragmentRoot root;
+
+	@BeforeEach
+	public void setup() throws CoreException {
+		root= context.createClasspathForJUnit(JUnitCore.JUNIT5_CONTAINER_PATH);
+	}
+
+	@Test
+	public void rejectsConfiguredFilterFieldsUntilDiscoveryTimeMappingExists() throws Exception {
+		ICompilationUnit performance= performance();
+		ICompilationUnit harness= harness();
+		ICompilationUnit filtered= unit("filtered", "FilteredTests.java", //$NON-NLS-1$ //$NON-NLS-2$
+				"""
+				package filtered;
+				public class FilteredTests extends org.eclipse.jdt.core.tests.junit.extension.TestCase {
+					static { TESTS_NUMBERS= new int[] { 2 }; }
+					public FilteredTests(String name) { super(name); }
+					public void test001() {}
+					public void test002() {}
+				}
+				""");
+
+		JdtCoreHarnessPlanner.Result result= plan(performance, harness, filtered);
+		Family family= family(result, "filtered.FilteredTests"); //$NON-NLS-1$
+		assertFalse(family.directSliceApplicable());
+		assertEquals("JDT_CORE_FILTER_MAPPING_REQUIRED", family.reasonCode()); //$NON-NLS-1$
+		assertTrue(result.directMigrations().isEmpty());
+	}
+
+	@Test
+	public void rejectsRunOnlyMethodNamesUntilDiscoveryTimeMappingExists() throws Exception {
+		ICompilationUnit performance= performance();
+		ICompilationUnit harness= harness();
+		ICompilationUnit filtered= unit("filtered", "RunOnlyTests.java", //$NON-NLS-1$ //$NON-NLS-2$
+				"""
+				package filtered;
+				public class RunOnlyTests extends org.eclipse.jdt.core.tests.junit.extension.TestCase {
+					public RunOnlyTests(String name) { super(name); }
+					public void testOrdinary() {}
+					public void testONLY_focus() {}
+				}
+				""");
+
+		JdtCoreHarnessPlanner.Result result= plan(performance, harness, filtered);
+		Family family= family(result, "filtered.RunOnlyTests"); //$NON-NLS-1$
+		assertFalse(family.directSliceApplicable());
+		assertEquals("JDT_CORE_FILTER_MAPPING_REQUIRED", family.reasonCode()); //$NON-NLS-1$
+		assertTrue(result.directMigrations().isEmpty());
+	}
+
+	private ICompilationUnit performance() throws CoreException {
+		return unit("org.eclipse.test.performance", "PerformanceTestCase.java", //$NON-NLS-1$ //$NON-NLS-2$
+				"""
+				package org.eclipse.test.performance;
+				public class PerformanceTestCase extends junit.framework.TestCase {
+					public PerformanceTestCase() {}
+					public PerformanceTestCase(String name) { super(name); }
+				}
+				""");
+	}
+
+	private ICompilationUnit harness() throws CoreException {
+		return unit("org.eclipse.jdt.core.tests.junit.extension", "TestCase.java", //$NON-NLS-1$ //$NON-NLS-2$
+				"""
+				package org.eclipse.jdt.core.tests.junit.extension;
+				public class TestCase extends org.eclipse.test.performance.PerformanceTestCase {
+					public static String RUN_ONLY_ID= "ONLY_";
+					public static String TESTS_PREFIX;
+					public static String[] TESTS_NAMES;
+					public static int[] TESTS_NUMBERS;
+					public static int[] TESTS_RANGE;
+					public TestCase(String name) { setName(name); }
+				}
+				""");
+	}
+
+	private JdtCoreHarnessPlanner.Result plan(ICompilationUnit... units) throws Exception {
+		return JdtCoreHarnessPlanner.create(context.getJavaProject(), units, parse(units), true, null);
+	}
+
+	private ICompilationUnit unit(String packageName, String fileName, String source) throws CoreException {
+		IPackageFragment fragment= root.createPackageFragment(packageName, true, null);
+		return fragment.createCompilationUnit(fileName, source, false, null);
+	}
+
+	private Map<String, CompilationUnit> parse(ICompilationUnit[] units) {
+		Map<String, CompilationUnit> roots= new LinkedHashMap<>();
+		ASTParser parser= ASTParser.newParser(IASTSharedValues.SHARED_AST_LEVEL);
+		parser.setProject(context.getJavaProject());
+		parser.setResolveBindings(true);
+		parser.setBindingsRecovery(IASTSharedValues.SHARED_BINDING_RECOVERY);
+		parser.setStatementsRecovery(IASTSharedValues.SHARED_AST_STATEMENT_RECOVERY);
+		parser.setCompilerOptions(RefactoringASTParser.getCompilerOptions(context.getJavaProject()));
+		parser.createASTs(units, new String[0], new ASTRequestor() {
+			@Override
+			public void acceptAST(ICompilationUnit source, CompilationUnit ast) {
+				roots.put(source.getPrimary().getHandleIdentifier(), ast);
+			}
+		}, null);
+		return roots;
+	}
+
+	private static Family family(JdtCoreHarnessPlanner.Result result, String typeName) {
+		return result.inventory().families().stream().filter(candidate -> typeName.equals(candidate.typeName()))
+				.findFirst().orElseThrow();
+	}
+}
