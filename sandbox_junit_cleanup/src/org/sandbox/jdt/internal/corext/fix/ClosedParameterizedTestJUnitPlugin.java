f8388ed9300a5a515d318efe890c2506bcb458d7
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-01 12:18:11 +0200

    Use the shared Parameterized eligibility contract

--- sandbox_junit_cleanup/src/org/sandbox/jdt/internal/corext/fix/ClosedParameterizedTestJUnitPlugin.java
+++ sandbox_junit_cleanup/src/org/sandbox/jdt/internal/corext/fix/ClosedParameterizedTestJUnitPlugin.java
@@ -12,11 +12,9 @@ package org.sandbox.jdt.internal.corext.fix;
 
 import static org.sandbox.jdt.internal.corext.fix.helper.lib.JUnitConstants.ORG_JUNIT_RUNWITH;
 
-import org.eclipse.jdt.core.dom.Annotation;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
+import org.sandbox.jdt.internal.corext.fix.helper.ParameterizedMigrationEligibility;
 import org.sandbox.jdt.internal.corext.fix.helper.ParameterizedTestJUnitPlugin;
 import org.sandbox.jdt.internal.corext.fix.helper.lib.JunitHolder;
 import org.sandbox.jdt.triggerpattern.api.CleanupPattern;
@@ -33,43 +31,14 @@ import org.sandbox.jdt.triggerpattern.api.PatternKind;
 		displayName = "JUnit 4 @RunWith(Parameterized) → JUnit 5 @ParameterizedTest")
 final class ClosedParameterizedTestJUnitPlugin extends ParameterizedTestJUnitPlugin {
 
-	private static final String PARAMETERS_ANNOTATION= "org.junit.runners.Parameterized.Parameters"; //$NON-NLS-1$
-
 	@Override
 	protected JunitHolder createHolder(Match match) {
 		JunitHolder holder= super.createHolder(match);
 		if (holder == null || !(holder.getAdditionalInfo() instanceof TypeDeclaration type)) {
 			return holder;
 		}
-		return hasSupportedLocalContract(type) ? holder : null;
-	}
-
-	private static boolean hasSupportedLocalContract(TypeDeclaration type) {
-		int providers= 0;
-		int constructors= 0;
-		boolean parameterizedConstructor= false;
-		for (MethodDeclaration method : type.getMethods()) {
-			if (method.isConstructor()) {
-				constructors++;
-				parameterizedConstructor= !method.parameters().isEmpty();
-				continue;
-			}
-			for (Object modifier : method.modifiers()) {
-				if (modifier instanceof Annotation annotation && isParametersAnnotation(annotation)) {
-					providers++;
-					break;
-				}
-			}
-		}
-		return providers == 1 && constructors == 1 && parameterizedConstructor;
-	}
-
-	private static boolean isParametersAnnotation(Annotation annotation) {
-		ITypeBinding binding= annotation.resolveTypeBinding();
-		if (binding != null) {
-			return PARAMETERS_ANNOTATION.equals(binding.getQualifiedName());
-		}
-		String name= annotation.getTypeName().getFullyQualifiedName();
-		return "Parameters".equals(name) || PARAMETERS_ANNOTATION.equals(name); //$NON-NLS-1$
+		return ParameterizedMigrationEligibility.assess(type).eligible()
+				? holder
+				: null;
 	}
 }
