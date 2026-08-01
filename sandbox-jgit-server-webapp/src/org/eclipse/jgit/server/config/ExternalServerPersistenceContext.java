9f25b6e32617d85044b97b0b2ba778b09f0af6db
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-01 11:49:42 +0200

    Preserve server bootstrap defaults in the external context

--- sandbox-jgit-server-webapp/src/org/eclipse/jgit/server/config/ExternalServerPersistenceContext.java
+++ sandbox-jgit-server-webapp/src/org/eclipse/jgit/server/config/ExternalServerPersistenceContext.java
@@ -33,12 +33,15 @@ import io.github.carstenartur.jgit.storage.hibernate.config.HibernateSessionFact
  */
 final class ExternalServerPersistenceContext implements ServerPersistenceContext {
 
+	private static final String LUCENE= "lucene"; //$NON-NLS-1$
+	private static final String LOCAL_FILESYSTEM= "local-filesystem"; //$NON-NLS-1$
+
 	private final HibernateSessionFactoryProvider owner;
 	private final SandboxRepositoryService repositories;
 
 	ExternalServerPersistenceContext(Properties properties) {
 		this(new HibernateSessionFactoryProvider(
-				copyOf(Objects.requireNonNull(properties, "properties")), //$NON-NLS-1$
+				serverProperties(Objects.requireNonNull(properties, "properties")), //$NON-NLS-1$
 				SandboxProjectionEntities.annotatedClasses()));
 	}
 
@@ -80,9 +83,38 @@ final class ExternalServerPersistenceContext implements ServerPersistenceContext
 		}
 	}
 
-	private static Properties copyOf(Properties properties) {
+	private static Properties serverProperties(Properties properties) {
 		Properties copy= new Properties();
 		copy.putAll(properties);
+
+		copy.putIfAbsent("hibernate.search.backend.type", LUCENE); //$NON-NLS-1$
+		String backend= copy.getProperty("hibernate.search.backend.type"); //$NON-NLS-1$
+		if (LUCENE.equalsIgnoreCase(backend)) {
+			copy.putIfAbsent("hibernate.search.backend.directory.type", //$NON-NLS-1$
+					LOCAL_FILESYSTEM);
+			if (LOCAL_FILESYSTEM.equalsIgnoreCase(copy.getProperty(
+					"hibernate.search.backend.directory.type"))) { //$NON-NLS-1$
+				String root= System.getenv("JGIT_SEARCH_INDEX_DIR"); //$NON-NLS-1$
+				if (root == null || root.isBlank()) {
+					root= "jgit-search-index"; //$NON-NLS-1$
+				}
+				copy.putIfAbsent("hibernate.search.backend.directory.root", root); //$NON-NLS-1$
+			}
+		}
+		copy.putIfAbsent("hibernate.search.backend.analysis.configurer", //$NON-NLS-1$
+				"class:org.eclipse.jgit.storage.hibernate.search.JavaSourceAnalysisConfigurer"); //$NON-NLS-1$
+
+		copy.putIfAbsent("hibernate.connection.provider_class", //$NON-NLS-1$
+				"org.hibernate.hikaricp.internal.HikariCPConnectionProvider"); //$NON-NLS-1$
+		copy.putIfAbsent("hibernate.hikari.minimumIdle", "5"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.hikari.maximumPoolSize", "20"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.hikari.idleTimeout", "300000"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.hikari.connectionTimeout", "20000"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.hikari.maxLifetime", "1200000"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.connection.handling_mode", //$NON-NLS-1$
+				"DELAYED_ACQUISITION_AND_RELEASE_AFTER_TRANSACTION"); //$NON-NLS-1$
+		copy.putIfAbsent("hibernate.cache.use_second_level_cache", "true"); //$NON-NLS-1$ //$NON-NLS-2$
+		copy.putIfAbsent("hibernate.cache.region.factory_class", "jcache"); //$NON-NLS-1$ //$NON-NLS-2$
 		return copy;
 	}
 }
