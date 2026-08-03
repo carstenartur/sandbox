29e6524d88b6dc426a6c72330cbc713d8f13ef76
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-03 22:01:19 +0200

    Use shared AST facts in array seed detector

--- sandbox_common_core/src/main/java/org/sandbox/jdt/container/analysis/AppendOnlyArraySeedDetector.java
+++ sandbox_common_core/src/main/java/org/sandbox/jdt/container/analysis/AppendOnlyArraySeedDetector.java
@@ -10,32 +10,32 @@
  *******************************************************************************/
 package org.sandbox.jdt.container.analysis;
 
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.assignment;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.expressionStatement;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.isArrayLength;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.isOne;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.nextStatement;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.sameVariable;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.unwrap;
+import static org.sandbox.jdt.container.analysis.ContainerAstFacts.variableBinding;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.eclipse.jdt.core.dom.ASTMatcher;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ArrayAccess;
 import org.eclipse.jdt.core.dom.Assignment;
-import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
-import org.eclipse.jdt.core.dom.FieldAccess;
-import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.NumberLiteral;
-import org.eclipse.jdt.core.dom.ParenthesizedExpression;
-import org.eclipse.jdt.core.dom.QualifiedName;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
-import org.eclipse.jdt.core.dom.SuperFieldAccess;
-import org.eclipse.jdt.core.dom.ArrayAccess;
-import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.sandbox.jdt.container.api.ContainerShape;
 import org.sandbox.jdt.container.api.ContainerUsageProfile;
 import org.sandbox.jdt.container.api.ContainerUsageProfile.AliasingContract;
@@ -75,8 +75,8 @@ public final class AppendOnlyArraySeedDetector {
 		ReferenceHolder<Integer, ContainerUsageProfile> profiles= ReferenceHolder.createIndexed();
 
 		AstProcessorBuilder.with(profiles)
-				.onAssignment((assignment, holder) -> {
-					detectSeed(assignment).ifPresent(profile ->
+				.onAssignment((candidate, holder) -> {
+					detectSeed(candidate).ifPresent(profile ->
 						holder.put(profile.identity().sourceStart(), profile));
 					return true;
 				})
@@ -94,7 +94,7 @@ public final class AppendOnlyArraySeedDetector {
 		}
 
 		Expression array= unwrap(growthAssignment.getLeftHandSide());
-		MethodInvocation copyOf= asMethodInvocation(growthAssignment.getRightHandSide());
+		MethodInvocation copyOf= methodInvocation(growthAssignment.getRightHandSide());
 		if (copyOf == null || !isArraysCopyOf(copyOf) || copyOf.arguments().size() != 2) {
 			return Optional.empty();
 		}
@@ -105,16 +105,16 @@ public final class AppendOnlyArraySeedDetector {
 			return Optional.empty();
 		}
 
-		ExpressionStatement growthStatement= asExpressionStatement(growthAssignment);
+		ExpressionStatement growthStatement= expressionStatement(growthAssignment);
 		Assignment appendAssignment= growthStatement == null
 				? null
-				: asAssignment(nextStatement(growthStatement));
+				: assignment(nextStatement(growthStatement));
 		if (!isTailWrite(appendAssignment, array)) {
 			return Optional.empty();
 		}
 
-		Optional<IVariableBinding> variableBinding= resolveVariableBinding(array);
-		ITypeBinding arrayType= variableBinding.map(IVariableBinding::getType)
+		Optional<IVariableBinding> binding= variableBinding(array);
+		ITypeBinding arrayType= binding.map(IVariableBinding::getType)
 				.orElseGet(array::resolveTypeBinding);
 		ElementDomain elementDomain= elementDomain(arrayType);
 		if (elementDomain == ElementDomain.PRIMITIVE) {
@@ -135,7 +135,7 @@ public final class AppendOnlyArraySeedDetector {
 		}
 
 		ContainerIdentity identity= new ContainerIdentity(
-				variableBinding.map(binding -> binding.getVariableDeclaration().getKey()).orElse(""), //$NON-NLS-1$
+				binding.map(value -> value.getVariableDeclaration().getKey()).orElse(""), //$NON-NLS-1$
 				array.toString(), array.getStartPosition(), array.getLength());
 
 		return Optional.of(new ContainerUsageProfile(
@@ -154,7 +154,7 @@ public final class AppendOnlyArraySeedDetector {
 				evidence));
 	}
 
-	private static UsageEvidence evidence(Kind kind, String summary, org.eclipse.jdt.core.dom.ASTNode node) {
+	private static UsageEvidence evidence(Kind kind, String summary, ASTNode node) {
 		return new UsageEvidence(kind, summary, node.getStartPosition(), node.getLength());
 	}
 
@@ -163,7 +163,7 @@ public final class AppendOnlyArraySeedDetector {
 			return ElementDomain.UNKNOWN;
 		}
 		if (!arrayType.isArray()) {
-			return ElementDomain.PRIMITIVE;
+			return ElementDomain.UNKNOWN;
 		}
 		ITypeBinding componentType= arrayType.getComponentType();
 		if (componentType == null) {
@@ -175,11 +175,11 @@ public final class AppendOnlyArraySeedDetector {
 		return componentType.isEnum() ? ElementDomain.ENUM : ElementDomain.REFERENCE;
 	}
 
-	private static boolean isTailWrite(Assignment assignment, Expression array) {
-		if (assignment == null || assignment.getOperator() != Assignment.Operator.ASSIGN) {
+	private static boolean isTailWrite(Assignment appendAssignment, Expression array) {
+		if (appendAssignment == null || appendAssignment.getOperator() != Assignment.Operator.ASSIGN) {
 			return false;
 		}
-		Expression leftHandSide= unwrap(assignment.getLeftHandSide());
+		Expression leftHandSide= unwrap(appendAssignment.getLeftHandSide());
 		if (!(leftHandSide instanceof ArrayAccess arrayAccess)) {
 			return false;
 		}
@@ -187,32 +187,9 @@ public final class AppendOnlyArraySeedDetector {
 				&& isLengthMinusOne(arrayAccess.getIndex(), array);
 	}
 
-	private static MethodInvocation asMethodInvocation(Expression expression) {
+	private static MethodInvocation methodInvocation(Expression expression) {
 		Expression unwrapped= unwrap(expression);
 		return unwrapped instanceof MethodInvocation invocation ? invocation : null;
-	}
-
-	private static ExpressionStatement asExpressionStatement(Assignment assignment) {
-		return assignment.getParent() instanceof ExpressionStatement statement ? statement : null;
-	}
-
-	private static Assignment asAssignment(Statement statement) {
-		if (!(statement instanceof ExpressionStatement expressionStatement)) {
-			return null;
-		}
-		Expression expression= unwrap(expressionStatement.getExpression());
-		return expression instanceof Assignment assignment ? assignment : null;
-	}
-
-	private static Statement nextStatement(Statement statement) {
-		if (!(statement.getParent() instanceof Block block)) {
-			return null;
-		}
-		List<?> statements= block.statements();
-		int index= statements.indexOf(statement);
-		return index >= 0 && index + 1 < statements.size()
-				? (Statement) statements.get(index + 1)
-				: null;
 	}
 
 	private static boolean isArraysCopyOf(MethodInvocation invocation) {
@@ -252,64 +229,5 @@ public final class AppendOnlyArraySeedDetector {
 			return false;
 		}
 		return isArrayLength(infix.getLeftOperand(), array) && isOne(infix.getRightOperand());
-	}
-
-	private static boolean isArrayLength(Expression expression, Expression array) {
-		Expression unwrapped= unwrap(expression);
-		if (unwrapped instanceof QualifiedName qualifiedName) {
-			return "length".equals(qualifiedName.getName().getIdentifier()) //$NON-NLS-1$
-					&& sameVariable(array, qualifiedName.getQualifier());
-		}
-		if (unwrapped instanceof FieldAccess fieldAccess) {
-			return "length".equals(fieldAccess.getName().getIdentifier()) //$NON-NLS-1$
-					&& sameVariable(array, fieldAccess.getExpression());
-		}
-		return false;
-	}
-
-	private static boolean isOne(Expression expression) {
-		Expression unwrapped= unwrap(expression);
-		Object constant= unwrapped.resolveConstantExpressionValue();
-		if (constant instanceof Number number) {
-			return number.longValue() == 1L;
-		}
-		return unwrapped instanceof NumberLiteral literal && "1".equals(literal.getToken()); //$NON-NLS-1$
-	}
-
-	private static boolean sameVariable(Expression first, Expression second) {
-		Expression left= unwrap(first);
-		Expression right= unwrap(second);
-		Optional<IVariableBinding> leftBinding= resolveVariableBinding(left);
-		Optional<IVariableBinding> rightBinding= resolveVariableBinding(right);
-		if (leftBinding.isPresent() && rightBinding.isPresent()) {
-			return leftBinding.get().getVariableDeclaration()
-					.isEqualTo(rightBinding.get().getVariableDeclaration());
-		}
-		return left.subtreeMatch(new ASTMatcher(true), right);
-	}
-
-	private static Optional<IVariableBinding> resolveVariableBinding(Expression expression) {
-		Expression unwrapped= unwrap(expression);
-		IBinding binding= null;
-		if (unwrapped instanceof SimpleName simpleName) {
-			binding= simpleName.resolveBinding();
-		} else if (unwrapped instanceof QualifiedName qualifiedName) {
-			binding= qualifiedName.resolveBinding();
-		} else if (unwrapped instanceof FieldAccess fieldAccess) {
-			binding= fieldAccess.resolveFieldBinding();
-		} else if (unwrapped instanceof SuperFieldAccess superFieldAccess) {
-			binding= superFieldAccess.resolveFieldBinding();
-		}
-		return binding instanceof IVariableBinding variableBinding
-				? Optional.of(variableBinding)
-				: Optional.empty();
-	}
-
-	private static Expression unwrap(Expression expression) {
-		Expression result= expression;
-		while (result instanceof ParenthesizedExpression parenthesized) {
-			result= parenthesized.getExpression();
-		}
-		return result;
 	}
 }
