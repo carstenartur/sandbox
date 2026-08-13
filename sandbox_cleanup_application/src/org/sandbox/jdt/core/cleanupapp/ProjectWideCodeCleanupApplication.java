3f542e36a5b1f2d2a00a5923c1b49dd57f5859ac
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-13 11:07:03 +0200

    Fix missing Set import in project cleanup diagnostics

--- sandbox_cleanup_application/src/org/sandbox/jdt/core/cleanupapp/ProjectWideCodeCleanupApplication.java
+++ sandbox_cleanup_application/src/org/sandbox/jdt/core/cleanupapp/ProjectWideCodeCleanupApplication.java
@@ -22,6 +22,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
