9e92947029aa663d1c032e90a8bcaee9db18e48b
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-17 11:26:50 +0200

    Expose coordinated multi-file encoding scope

--- sandbox_encoding_quickfix/src/org/sandbox/jdt/internal/ui/fix/UseExplicitEncodingCleanUp.java
+++ sandbox_encoding_quickfix/src/org/sandbox/jdt/internal/ui/fix/UseExplicitEncodingCleanUp.java
@@ -13,22 +13,39 @@
  *******************************************************************************/
 package org.sandbox.jdt.internal.ui.fix;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.internal.ui.fix.AbstractCleanUpCoreWrapper;
+
+import org.sandbox.jdt.cleanup.multifile.api.IMultiFileCleanUpScopeProvider;
+
 /**
- * Until
- * https://openjdk.java.net/jeps/400
- * is active platform encoding might be different than UTF-8.
- * So it can be dangerous not to use explicit encoding.
+ * Until JEP 400 is active, platform encoding might be different than UTF-8.
+ * Explicit encoding also enables migration to the type-safe modern APIs.
  */
-public class UseExplicitEncodingCleanUp extends AbstractCleanUpCoreWrapper<UseExplicitEncodingCleanUpCore> {
+public class UseExplicitEncodingCleanUp extends AbstractCleanUpCoreWrapper<UseExplicitEncodingCleanUpCore>
+		implements IMultiFileCleanUpScopeProvider {
 	public UseExplicitEncodingCleanUp(final Map<String, String> options) {
 		super(options, new UseExplicitEncodingCleanUpCore());
 	}
 
 	public UseExplicitEncodingCleanUp() {
 		this(Collections.emptyMap());
+	}
+
+	@Override
+	public Collection<ICompilationUnit> expandCleanUpScope(IJavaProject project,
+			Collection<ICompilationUnit> currentScope, IProgressMonitor monitor) throws CoreException {
+		return cleanUpCore.expandCleanUpScope(project, currentScope, monitor);
+	}
+
+	public Collection<Map<String, Object>> getCoordinatedCleanUpPreview(IJavaProject project) throws CoreException {
+		return cleanUpCore.getCoordinatedCleanUpPreview(project);
 	}
 }
